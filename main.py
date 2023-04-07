8a6175c338fb0e26f7a653a6ffe171103aa48945
Carlos Roberto <48866075+carlosr-henriques@users.noreply.github.com>
2023-04-07 03:25:04 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-from request_ability import get_ability
+from ability import get_ability
+from pokemon import get_all_pokemon
+from IPython.display import display
+from pandas import set_option
+set_option('display.max_rows', 1300)
 
 def main():
-    pokemon = input("Please inform the pokemon you want know the abilities: ")
-    get_ability(pokemon=pokemon)
+    print("What do you want?", "1 - List all pokemons", "2 - List a pokemon's abilities")
+    response = input("Digit: ")
+    if response == '1':
+        df = get_all_pokemon()
+        display(df['name'])
+    else:
+        pokemon = input("Please inform the pokemon you want know the abilities: ")
+        get_ability(pokemon=pokemon)
 
 if __name__ == '__main__':
     main()
